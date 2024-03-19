c386c83fbbf04543819416966e7500c1883739ed
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-19 08:12:05 -0300

    added the 'check' function

--- src/app/palindrome-checker/palindrome-checker.component.ts
+++ src/app/palindrome-checker/palindrome-checker.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-palindrome-checker',
   templateUrl: './palindrome-checker.component.html',
-  styleUrl: './palindrome-checker.component.css'
+  styleUrl: './palindrome-checker.component.css',
 })
 export class PalindromeCheckerComponent {
+  word: string = '';
+  result: boolean = false;
 
+  check() {
+    if (this.word.length >= 3) {
+      const cleanWord = this.word.toLowerCase();
+      const invertedWord = cleanWord.split('').reverse().join('');
+      return this.result = cleanWord === invertedWord;
+    }else{
+      this.result = false
+      return this.result
+    }
+  }
 }
